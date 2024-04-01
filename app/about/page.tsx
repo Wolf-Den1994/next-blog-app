223489b71b2497e5a531afbefd5e41d925a5b604
Denis K <wolf-den@tut.by>
2024-04-01 13:29:06 +0300

    feat: add metadata title

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About | Next Blog",
+};
+
 export default function About() {
   return <h3>Select subitem</h3>
 }
