546bb3b8fdeeae5ac776961104dc9269ba1aa598
Denis K <wolf-den@tut.by>
2024-04-03 17:43:09 +0300

    feat: POST request and body

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -4,3 +4,11 @@ import { posts } from "./posts";
 export async function GET(req: Request) {
   return NextResponse.json(posts);
 }
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  console.log('body', body);
+
+  return NextResponse.json({ body });
+}
