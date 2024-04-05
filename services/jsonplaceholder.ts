0cb1711384a9b2626b0a51522bfc99e389525bf9
Denis K <wolf-den@tut.by>
2024-04-05 15:12:03 +0300

    feat: add ISR

--- services/jsonplaceholder.ts
+++ services/jsonplaceholder.ts
@@ -2,18 +2,18 @@ import type { Post } from "@/types/post"
 
 export async function getAllPosts(): Promise<Post[]> {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    next: {
-      revalidate: 60, // 1min
-    }
+    // next: {
+      // revalidate: 60, // 1min
+    // },
   });
   return response.json();
 }
 
 export async function getData(id: string): Promise<Post> {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-    next: {
-      revalidate: 60, // 1min
-    }
+    // next: {
+    //   revalidate: 60, // 1min
+    // }
   });
   return response.json();
 }
