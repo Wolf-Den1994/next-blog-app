3899a4265f736d477b1852e5d0c34d4a69c79e8b
Denis K <wolf-den@tut.by>
2024-04-06 13:37:21 +0300

    feat: api images

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import CatImg from '@/assets/cat.png'
 import { Image } from '@/components/Image'
+import { getPhotos } from '@/services/jsonplaceholder';
 
-export default function Home() {
+export default async function Home() {
+  const photos = await getPhotos()
+
   return (
     <>
       <h1>Welcome</h1>
@@ -9,8 +12,25 @@ export default function Home() {
         src={CatImg}
         alt="cat"
         priority
+        placeholder="blur"
+        style={{
+          maxWidth: '100%',
+          height: 'auto',
+        }}
+        quality={80}
       />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempora cumque accusamus. Tempora, dolores quia quidem repudiandae doloribus minima rem officiis nihil consectetur nisi illum similique impedit at nemo ipsum?</p>
+      {photos.map((photo) => (
+        <Image
+          key={photo.id}
+          src={photo.url}
+          alt={photo.title}
+          width={300}
+          height={100}
+        />
+      ))}
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nihil harum eum laudantium similique a sint adipisci commodi. Nobis recusandae temporibus optio ipsam culpa magnam laudantium dolores? Dolorem, officiis tenetur!
+      Dolore accusamus ipsum suscipit ex quod nostrum sit eaque placeat repellat ea quo rem distinctio corrupti eum exercitationem, provident minima dignissimos neque sunt facere quisquam accusantium ad numquam obcaecati. Molestiae?</p>
     </>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    // deviceSizes: [375, 640, 750, 828, 1080, 1200, 1920],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+      }
+    ]
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  swcMinify: true,
+};
 
 export default nextConfig;
