0cf0d408bdbda412f0479266fa06d64d07a43084
Denis K <wolf-den@tut.by>
2024-04-06 13:15:46 +0300

    local images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import CatImg from '@/assets/cat.png'
+import { Image } from '@/components/Image'
+
 export default function Home() {
   return (
-    <h1>Welcome</h1>
+    <>
+      <h1>Welcome</h1>
+      <Image
+        src={CatImg}
+        alt="cat"
+        priority
+      />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempora cumque accusamus. Tempora, dolores quia quidem repudiandae doloribus minima rem officiis nihil consectetur nisi illum similique impedit at nemo ipsum?</p>
+    </>
   );
 }
