efe6523af9bb17a0e4b91dd1958a05746f30fcbe
Denis K <wolf-den@tut.by>
2024-04-08 12:28:28 +0300

    feat: add create new post on blog page and other paths

--- app/blog/new/page.tsx
+++ app/blog/new/page.tsx
@@ -1,10 +1,15 @@
 import NewPostForm from "@/components/NewPostForm";
+import { redirect } from "next/navigation";
 
 export default function CreatePost() {
   return (
     <div className="content">
       <h1>Create new post</h1>
-      <NewPostForm />
+      <NewPostForm onCreated={async (id) => {
+        'use server';
+
+        redirect(`/blog/${id}`)
+      }} />
     </div>
   )
 }
--- components/NewPostForm.tsx
+++ components/NewPostForm.tsx
@@ -1,24 +1,24 @@
-import { redirect } from "next/navigation";
+export default function NewPostForm({ onCreated }: {
+  onCreated: (id?: number) => Promise<void>
+}) {
+  async function createPost(data: FormData) {
+    'use server';
+  
+    const { title, body } = Object.fromEntries(data);
+  
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, body, userId: 1 })
+    })
+  
+    const post = await response.json();
 
-async function createPost(data: FormData) {
-  'use server';
+    await onCreated(post.id);
+  }
 
-  const { title, body } = Object.fromEntries(data);
-
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ title, body, userId: 1 })
-  })
-
-  const post = await response.json();
-
-  redirect(`/blog/${post.id}`)
-}
-
-export default function NewPostForm() {
   return (
     <form className="content-form" action={createPost}>
       <input type="text" name="title" placeholder="title" required />
