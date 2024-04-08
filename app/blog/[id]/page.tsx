1d816d0846dff9202de92bdfbcd4a91184cb583a
Denis K <wolf-den@tut.by>
2024-04-08 12:33:22 +0300

    feat: add remove posts

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { getAllPosts, getData } from "@/services/jsonplaceholder"
 import { Metadata } from "next"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 
 type Props = {
   params: {
@@ -23,11 +25,26 @@ export async function generateMetadata({ params: { id } }: Props): Promise<Metad
   }
 }
 
+async function removePosts(id: string) {
+  'use server';
+
+  await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    method: 'DELETE',
+  })
+
+  revalidatePath('/blog');
+  redirect('/blog')
+}
+
 export default async function Post({ params: { id } }: Props) {
   const post = await getData(id);
 
   return <>
     <h1>{post.title}</h1>
     <p>{post.body}</p>
+
+    <form action={removePosts.bind(null, id)}>
+      <input type="submit" value="delete post" />
+    </form>
   </>
 }
